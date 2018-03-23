c9aa09b00e892fbccc002b5fa41562adfdaeb608
niksmitty <niksmitty@rambler.ru>
2018-03-23 15:46:17 +0500

    Added response type - image.

--- app/routes/image_routes.js
+++ app/routes/image_routes.js
@@ -1,6 +1,7 @@
 module.exports = function(app, gs) {
   app.get('/getImageLink', (req, res) => {
     const searchText = req.query.searchText;
+    const responseType = req.query.responseType;
     gs.build({
       q:searchText,
       searchType: 'image'
@@ -14,7 +15,16 @@ module.exports = function(app, gs) {
       if (items !== 'undefined' && items.length > 0) {
         let firstItem = items[0];
         if (items[0].hasOwnProperty('link')) {
-          res.send(firstItem['link']);
+          if (responseType === 'image') {
+            res.send(`<svg width="100%" height="100%" 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    xmlns:xlink="http://www.w3.org/1999/xlink">       
+                    <image xlink:href="${ firstItem['link'] }" x="0" y="0" height="200" width="200" />    
+                    </svg>`
+                  );
+          } else {
+            res.send(firstItem['link']);
+          }
         }
       } else {
         res.send('Items are missing...');
